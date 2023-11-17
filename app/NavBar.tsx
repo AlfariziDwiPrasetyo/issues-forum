fe288bed428b8c804fee345fbf34709d5bc82c0f
Ryuugamine <yoerdanatan@gmail.com>
2023-11-17 10:26:01 +0700

    styling active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,18 +1,25 @@
+'use client'
+
 import React from 'react'
 import { AiOutlineQq } from "react-icons/ai";
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 const NavBar = () => {
+    const currentPath = usePathname()
     const links = [
         {label:"Home", url:"/"},
         {label:"Issues", url:"/issues"},
     ]
   return (
-    <nav className='flex space-x-6 mb-4 border-b px-6 h-14 items-center'>
-        <p><AiOutlineQq/></p>
+    <nav className='flex space-x-6 mb-5 border-b px-6 h-14 items-center'>
+        <p><Link href="/"><AiOutlineQq/></Link></p>
         <ul className='flex space-x-6'>
-           <li><Link href="/" className='text-zinc-500 hover:text-zinc-800'>Home</Link></li>
-            <li><Link href='/issues' className='text-zinc-500 hover:text-zinc-800'>Issues</Link></li>
+           {links.map(link => <Link key={link.url} href={link.url} className={classNames({
+            'text-zinc-900': currentPath == link.url,
+            'text-zinc-500': currentPath !== link.url,
+            'hover:text-zinc-800 trasition-colors':true
+           })}>{link.label}</Link>)}
         </ul>
     </nav>
   )
